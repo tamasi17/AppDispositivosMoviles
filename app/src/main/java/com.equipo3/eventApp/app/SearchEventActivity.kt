4cfb49402d4ab3a72ff27adea11081a011c145e5
sofiipz <sofiapdam@gmail.com>
2025-12-16 19:57:45 +0100

    feat(searchEvent): Añadida SearchEventActivity y documentación básica

---
+++ app/src/main/java/com.equipo3/eventApp/app/SearchEventActivity.kt
@@ -0,0 +1,8 @@
+/* Activity para buscar eventos y mostrar resultados */
+package com.equipo3.eventApp.app
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchEventActivity : AppCompatActivity() {
+    // Aquí iría el código de la Activity
+}
