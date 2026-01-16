81e120d0d6fe1c5ff027a6144a0e97158081ac05
sofiipz <sofiapdam@gmail.com>
2026-01-16 20:42:46 +0100

    (Color.kt) added purple, black and white colors codes

--- MaccsEvents/app/src/main/java/com/maccs/events/ui/theme/Color.kt
+++ MaccsEvents/app/src/main/java/com/maccs/events/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val DarkPurple = Color(0xFF4D0372)
+val LigthPurple = Color(0xFF9333EA)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
