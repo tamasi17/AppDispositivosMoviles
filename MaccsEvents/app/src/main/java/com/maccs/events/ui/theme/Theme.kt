d6bb34a87daab36ecb351898386992c79d07b1fd
cebriii <daceherx@gmail.com>
2026-01-23 20:36:05 +0100

    (Theme.kt) Modified darkTheme val to TRUE instead of isSystemInDarkTheme

--- MaccsEvents/app/src/main/java/com/maccs/events/ui/theme/Theme.kt
+++ MaccsEvents/app/src/main/java/com/maccs/events/ui/theme/Theme.kt
@@ -35,7 +35,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun MaccsEventsTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = true,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
