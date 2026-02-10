5ff6c62587353383948babc00400a132b567fe67
mats <mat.eidelman@gmail.com>
2026-02-10 23:03:22 +0100

    MainActivity includes FirebaseAuth, report.csv with commits

--- MaccsEvents/app/src/main/java/com/maccs/events/MainActivity.kt
+++ MaccsEvents/app/src/main/java/com/maccs/events/MainActivity.kt
@@ -3,6 +3,7 @@ package com.maccs.events
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.maccs.events.ui.auth.LoginActivity
 import com.maccs.events.ui.home.HomeActivity // Asumiendo que existe
 
@@ -10,24 +11,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // AQUÍ VA TU LÓGICA DE ENRUTAMIENTO
+        // 1. Preguntamos a Firebase si existe el usuario de test
+        val currentUser = FirebaseAuth.getInstance().currentUser
 
-        // 1. (Opcional) Comprobar si hay usuario guardado (Simulado por ahora)
-        val isUserLoggedIn = false
-
-        val intent = if (isUserLoggedIn) {
-            // Si ya está logueado, vamos directo al Home
+        val intent = if (currentUser != null) {
+            // USUARIO LOGUEADO -> Vamos al Home
             Intent(this, HomeActivity::class.java)
         } else {
-            // Si no, vamos al Login
+            // NADIE LOGUEADO -> Vamos al Login
             Intent(this, LoginActivity::class.java)
         }
 
-        // 2. Arrancar la actividad
+        // 2. Iniciamos la actividad correspondiente
         startActivity(intent)
 
-        // 3. ¡IMPORTANTE! Matar la MainActivity para que el usuario
-        // no pueda volver a ella pulsando "Atrás"
+        // 3. ¡IMPORTANTE! Matamos esta Activity para que el usuario no pueda volver atrás
         finish()
     }
 }
