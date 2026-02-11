3ea5d998a4c48cb12cbb6e9a08ef83b3bc4b15c5
Ceeciimg <molinagarciainformatica@gmail.com>
2026-02-11 22:22:34 +0100

    ajustar comentarios

--- MaccsEvents/app/src/main/java/com/maccs/events/MainActivity.kt
+++ MaccsEvents/app/src/main/java/com/maccs/events/MainActivity.kt
@@ -5,27 +5,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.maccs.events.ui.auth.LoginActivity
-import com.maccs.events.ui.home.HomeActivity // Asumiendo que existe
+import com.maccs.events.ui.home.HomeActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 1. Preguntamos a Firebase si existe el usuario de test
+
         val currentUser = FirebaseAuth.getInstance().currentUser
 
         val intent = if (currentUser != null) {
-            // USUARIO LOGUEADO -> Vamos al Home
+
             Intent(this, HomeActivity::class.java)
         } else {
-            // NADIE LOGUEADO -> Vamos al Login
+
             Intent(this, LoginActivity::class.java)
         }
 
-        // 2. Iniciamos la actividad correspondiente
+
         startActivity(intent)
 
-        // 3. ¡IMPORTANTE! Matamos esta Activity para que el usuario no pueda volver atrás
         finish()
     }
 }
