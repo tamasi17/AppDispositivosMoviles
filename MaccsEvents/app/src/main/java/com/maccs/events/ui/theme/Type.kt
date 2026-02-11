2cac3479b66a0d440f392d2e8ad87fee803dd13d
anapope <ana.moreno.mampel@gmail.com>
2026-02-11 17:01:21 +0100

    Changed font to bold, semibold and extrabold

--- MaccsEvents/app/src/main/java/com/maccs/events/ui/theme/Type.kt
+++ MaccsEvents/app/src/main/java/com/maccs/events/ui/theme/Type.kt
@@ -2,27 +2,42 @@ package com.maccs.events.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.font.Font
-import com.maccs.events.R.font
+import com.maccs.events.R
 
-// Set of Material typography styles to start with
-// ui/theme/Type.kt
+// Definimos la familia de fuentes
 val NunitoFamily = FontFamily(
-    Font(font.nunito_regular, FontWeight.Normal),
-    Font(font.nunito_regular, FontWeight.Bold)
+    // Al poner FontWeight.Normal vinculado al archivo bold.ttf,
+    // engañamos al sistema para que use Bold siempre.
+    Font(R.font.nunito_semibold, FontWeight.Normal),
+    Font(R.font.nunito_extrabold, FontWeight.Bold),
+    Font(R.font.nunito_semibold, FontWeight.Medium)
 )
 
+// Configuramos la tipografía base de Material3
 val Typography = Typography(
-    // Estilo para los títulos de las barras superiores
+    bodyLarge = TextStyle(
+        fontFamily = NunitoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
     titleLarge = TextStyle(
         fontFamily = NunitoFamily,
         fontWeight = FontWeight.Bold,
-        fontSize = 24.sp, // El tamaño concreto que querías
+        fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    // Puedes añadir bodyLarge, labelSmall, etc.
+    labelSmall = TextStyle(
+        fontFamily = NunitoFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
 )
